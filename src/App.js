760507f27208483f73c4a15681ce8d0d47d58e83
robbie-ellis <128123325+robbie-ellis@users.noreply.github.com>
2024-07-09 11:04:39 -0700

    Added html button and label elements and css styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='App-header'>Pomodoro Clock</h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6 remove-padding'><label className='w-100' id='break-label'>Break Length</label></div>
+          <div className='col-sm-6 remove-padding'><label className='w-100' id='session-label'>Session Length</label></div>
+        </div>
+        <div className='row'>
+          <div className='col-sm-2 remove-padding'><button className='w-100' id='break-decrement'>-</button></div>
+          <div className='col-sm-2'><label id='break-length' value='5' className='w-100'>5</label></div>
+          <div className='col-sm-2 remove-padding'><button className='w-100' id='break-increment'>+</button></div>
+          <div className='col-sm-2 remove-padding'><button className='w-100' id='break-decrement'>-</button></div>
+          <div className='col-sm-2'><label id='session-length' value='25' className='w-100'>25</label></div>
+          <div className='col-sm-2 remove-padding'><button className='w-100' id='break-increment'>+</button></div>
+        </div>
+      </div>
     </div>
   );
 }
